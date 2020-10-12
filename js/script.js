3effcb7508389bbeead3f8851ba7fe531eab4792
carmine19 <carminepepiciello@gmail.com>
2020-10-12 15:51:14 +0200

    aggiunto numero random con ora attuale

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 
 
+    // let bodyBackground = document.getElementById('body-box');
+
+
+    /* creo la data */
+    const data = new Date();
+    console.log(data)
+
+    let actualHours = data.getHours();
+    console.log(actualHours)
+
+    let actualMinutes = data.getMinutes();
+    console.log(actualMinutes)
+
+    let actualSeconds = data.getSeconds();
+    console.log(actualSeconds)
+
+    let youLocalHours = document.getElementById('data').innerHTML ='Sono le ore:'+ '&nbsp' + actualHours + ':' + actualMinutes + ':' + actualSeconds;
+    /* fine data */
+
     
+    /* creo il generatore */
 
     let name = prompt('Come ti chiami?');
     console.log('ti chiami:' + name);
@@ -11,12 +31,16 @@
     let preferdColours = prompt('ed il tuo colore preferito?')
     console.log('il tuo colore preferito:' + preferdColours);
 
+    let randomNumber = Math.floor(Math.random() * 1989);
+    console.log(randomNumber)
 
 
-    const passwordUser = document.getElementById('password').innerHTML = name + surname + preferdColours;
+
+    const passwordUser = document.getElementById('password').innerHTML = name + surname + preferdColours + randomNumber;
 
     let passwordWhitText = document.getElementById('password').innerHTML = 'La tua password generatà è:' + '&nbsp'+ passwordUser;
 
+    /* fine generatore */
 
 
 
